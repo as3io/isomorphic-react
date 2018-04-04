c76330a8f8ce0643a43740f4c271b414bbe6f3b0
Jacob Bare <jacob@limit0.io>
2018-04-04 11:59:13 -0500

    Add webpack cli, config, and simple prod build

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const { resolve } = path;
+
+const srcDir = resolve(__dirname, 'src');
+const buildDir = resolve(__dirname, 'dist');
+const nodeModules = resolve(__dirname, 'node_modules');
+
+module.exports = () => {
+  return {
+    devtool: 'inline-source-map',
+    entry: [
+      resolve(srcDir, 'index.js')
+    ],
+    output: {
+      path: buildDir,
+      filename: 'app.[hash].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          include: [ srcDir ],
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env', 'react'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
